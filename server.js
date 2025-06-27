ed234e8de8aefc49e6e7aadc16f769cd1ff4db9b
AsheinTech <asheintechnologies@gmail.com>
2025-06-27 14:06:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { Configuration, OpenAIApi } = require('openai');
+const OpenAI = require("openai");
 
 const app = express();
 const port = process.env.PORT || 5000;
