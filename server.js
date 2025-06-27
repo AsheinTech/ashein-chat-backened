f41a6e1580c5cbf8e61853dac9655353c6e3d559
AsheinTech <asheintechnologies@gmail.com>
2025-06-27 21:37:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post('/chat', async (req, res) => {
         'X-Title': 'Ashein AI Chatbot'
       },
       body: JSON.stringify({
-        model: 'openrouter/cinematika-7b',
+        model: 'mistralai/mistral-small-3.2-24b-instruct:free',,
         messages: [{ role: 'user', content: message }]
       })
     });
